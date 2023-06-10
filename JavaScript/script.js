7b639709b0f1103ff3921ed8d67d5ecb71a97249
MaryAznivG <marygalstyan10@gmail.com>
2023-06-09 22:13:23 -0700

    added link to all cities

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -34,7 +34,8 @@ function updateCity(event){
       <h2>${cityName}</h2>
       <div class="date">${cityTime.format("MMMM  Do YYYY")}</div>
       <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small></div>
-    </div> `;
+    </div>
+    <a href="/">All cities<a/> `;
 }
 
 updateTime();
