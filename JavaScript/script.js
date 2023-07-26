bd02e0f3607b183e07babb30a69a6f57f92a8fa4
MaryAznivG <marygalstyan10@gmail.com>
2023-07-25 21:16:18 -0700

    revising my code and correcting for seconds  in the cities

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -21,26 +21,36 @@ parisTimeElement.innerHTML=parisTime.format("h:mm:ss [<small>]A[</small>]");
 }
 }
 
-function updateCity(event){
-    let cityTimeZone = event.target.value;
-    if (cityTimeZone === "current"){
-        cityTimeZone = moment.tz.guess();
-    }
-    let cityName=cityTimeZone.replace("_"," ").split("/")[1];
-    let cityTime = moment().tz(cityTimeZone);
-    let citiesElement=document.querySelector("#cities");
-    citiesElement.innerHTML= ` 
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = ` 
     <div class="city">
+    <div>
       <h2>${cityName}</h2>
       <div class="date">${cityTime.format("MMMM  Do YYYY")}</div>
-      <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small></div>
+      </div>
+      <div class="time">${cityTime.format("h:mm:ss[<small>]A[</small>]")}</div> 
     </div>
-    <a href="/">Back to homepage<a/> `;
+    <div class="text-center">
+    <a href="/">Back to homepage<a/>
+    </div> 
+     `;
+  setTimeout(() => {
+    updateCity(event);
+  }, 1000);
 }
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
+
 
 updateTime();
 setInterval(updateTime,1000);
 
 
-let citiesSelectElement=document.querySelector("#city");
-citiesSelectElement.addEventListener("change", updateCity);
+
